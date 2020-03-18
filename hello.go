f86d4e52a7b8bef5356a8f03923f9416347f384a
Jay Stewart <jayr.stewart@live.com>
2020-03-17 20:33:31 -0400

    whatt

--- hello.go
+++ hello.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
 
 func ReverseRunes(s string) string {
